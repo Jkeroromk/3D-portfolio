22cf998a12970b874ada1e3099877c114e7dd15d
Jkeroro <104914788+Jkeroromk@users.noreply.github.com>
2025-01-26 16:32:00 -0500

    Update Experience.jsx
    update my personal link in bio website

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -199,7 +199,7 @@ export default function Experience({ started, onToggleNav }) {
               rotation-x={-0.256}
               style={{ opacity }}
             >
-              <iframe src="https://jkeroro-website.vercel.app/" />
+              <iframe src="https://jkeroro.com" />
             </Html>
 
             {/* Text Elements */}
